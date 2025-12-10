455d67809eadec58f8f423ed6e3fe158d1a7a619
aadisoni8044-code <aadisoni8044@gmail.com>
2025-12-10 14:17:40 +0530

    Implement LED blinking on ESP32
    Add LED blinking functionality using ESP32 GPIO.

--- micro python.py
+++ micro python.py
@@ -1,1 +1,13 @@
-print("aaa")
+from machine import Pin
+from time import sleep
+
+led = Pin(2, Pin.OUT)   # ESP32/ में PIN 2 पर LED
+
+while True:
+    led.value(1)
+    sleep(0.5)
+    led.value(0)
+    sleep(0.5)
+
+#LED = ON ---------------------|| file esp.py
+#LED = OFF
